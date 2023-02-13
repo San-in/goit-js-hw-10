1f9d43f663a91f29015b7c6b28727e3cde56a889
Sanin Oleksandr <saninalexsanin@gmail.com>
2023-02-13 16:14:01 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ const debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
 const linkOnSearchForm = document.getElementById('search-box');
 
-
 linkOnSearchForm.addEventListener('input',debounce(handleOnSearch,DEBOUNCE_DELAY));
 
 
